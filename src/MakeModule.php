a275b67898284806750f63c607a0a7eec7403407
Angelo Joseph Salvador <angelo.iopenhub@gmail.com>
2018-01-12 08:09:52 +0000

    Test run

--- src/MakeModule.php
+++ src/MakeModule.php
@@ -2,8 +2,25 @@
 
 namespace Angelo8828\MakeModule;
 
+use Illuminate\Console\Command;
+
 class MakeModule
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:module
+                           {name : The name of the module. Must be written in studly (upper camel) case if two or three more words}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new module';
+
     /**
      * Create a new command instance.
      *
@@ -12,5 +29,15 @@ class MakeModule
     public function __construct()
     {
         parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        echo "Hello World!";
     }
 }
