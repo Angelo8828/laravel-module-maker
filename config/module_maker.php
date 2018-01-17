666a1590b093880a20ca812b37f923ec5ef7193f
Angelo Joseph Salvador <angelo.iopenhub@gmail.com>
2018-01-17 07:26:03 +0000

    Modified config file

--- config/module_maker.php
+++ config/module_maker.php
@@ -2,20 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Route File
-    |--------------------------------------------------------------------------
-    |
-    | This value determines the route file that the Laravel Module Maker will
-    | utilize. The auto-generated routes will be placed at the bottom of the
-    | route file each time the command will be runned. The default value is the
-    | location of the route file for Laravel 5.5
-    |
-    */
-
-    'route_file' => env('MODULE_MAKE_ROUTE_FILE', 'routes/web.php'),
-
 	/*
     |--------------------------------------------------------------------------
     | Model Namespace
@@ -35,7 +21,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Is Migration Enabled
+    | Is Migration Enabled?
     |--------------------------------------------------------------------------
     |
     | This value determines if auto-generation of database migration will be enabled.
@@ -45,6 +31,19 @@ return [
     */
 
     'is_migration_enabled' => env('MODULE_MAKE_IS_MIGRATION_ENABLED', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Is Resource Routing Enabled?
+    |--------------------------------------------------------------------------
+    |
+    | This value determines if Laravel resource routing is enabled. If enabled, controllers
+    | with full resource routes will be auto-generated. Please see
+    | https://laravel.com/docs/5.5/controllers#resource-controllers for documentation guide.
+    | False by default.
+    |
+    */
+    'is_resource_routing_enabled' => env('MODULE_MAKE_IS_RESOURCE_ROUTING_ENABLED', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -63,4 +62,31 @@ return [
     */
 
     'controller_namespace' => env('MODULE_MAKE_CONTROLLER_NAMESPACE', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Route Letter Case Naming Convention
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the route letter case naming convention that will be applied
+    | and followed in the auto-generation of routes. Only two letter cases, "slug" and "snake"
+    | are currently acceptable. The default case will be "slug".
+    |
+    */
+
+    'route_letter_case_naming_convention' => env('MODULE_MAKE_ROUTE_LETTER_CASE_NAMING_CONVENTION', 'slug'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Route File
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the route file that the Laravel Module Maker will
+    | utilize. The auto-generated routes will be placed at the bottom of the
+    | route file each time the command will be runned. The default value is the
+    | location of the route file for Laravel 5.5
+    |
+    */
+
+    'route_file' => env('MODULE_MAKE_ROUTE_FILE', 'routes/web.php'),
 ];
